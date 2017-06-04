c45785680c62d4c2feaf50ba8c0fdd854f0d7bfc
zhengooxingoo <zhengooxingoo@gmial.com>
2017-06-04 16:10:01 +0800

    html 编写表单，发送POST请求,服务器view中处理请求，并把python变量传入到模板html中渲染

--- functional_tests.py
+++ functional_tests.py
@@ -30,10 +30,16 @@ class NewVisitorTest(unittest.TestCase):
         
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows),
-            'New to-do list did not appear in table'
-        )
+        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        self.assertIn('2:Use peacock feathers to make a fly',[row.text for row in rows])
         
         self.fail("Finish the test!")
         
--- lists/tests.py
+++ lists/tests.py
@@ -3,9 +3,21 @@ from lists.views import home_page
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+import re
+
+
 # Create your tests here.
 class HomePageTest(TestCase):
 
+    def remove_csrf(self,html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
+
+    def assertEqualExceptCSRF(self, html_code1, html_code2):
+        return self.assertEqual(
+            self.remove_csrf(html_code1),
+            self.remove_csrf(html_code2)
+        )
     def test_root_url_resolve_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -14,5 +26,18 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
-        self.assertEqual(expected_html,response.content.decode())
+        self.assertEqualExceptCSRF(expected_html,response.content.decode())
+        
+    def test_home_page_can_save_a_POST_request(self):
+        request =HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] ='A new list item'
+        
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text':'A new list item'}
+        )
+        self.assertEqualExceptCSRF(response.content.decode(),expected_html)
         
