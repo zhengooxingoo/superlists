a54f1590ee183c5d14aba84bc82f247972424a29
zhengooxingoo <zhengooxingoo@gmial.com>
2017-06-05 15:57:13 +0800

    添加了List模型作为item的外键，让待办项和不同的清单关联起来

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 import re
-from lists.models import Item
+from lists.models import Item,List
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -29,16 +29,24 @@ class HomePageTest(TestCase):
         self.assertEqualExceptCSRF(expected_html,response.content.decode())
         
         
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+        
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
         
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+        
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list,list_)
         
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(),2)
@@ -46,7 +54,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list,list_)
         self.assertEqual(second_saved_item.text,'Item the second')
+        self.assertEqual(second_saved_item.list,list_)
               
 class LiveViewTest(TestCase):
 
@@ -55,8 +65,9 @@ class LiveViewTest(TestCase):
         self.assertTemplateUsed(response,'list.html')
 
     def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1',list=list_)
+        Item.objects.create(text='itemey 2',list= list_)
         
         response = self.client.get('/lists/the-only-list-in-the-world/')
         
