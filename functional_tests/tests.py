f972d9ff3fc02364c318f9b3fe2d9665c3524e8c
zhengooxingoo <zhengooxingoo@gmial.com>
2017-06-07 08:55:27 +0800

    modify the functional_tests by using tengxun cloud host, and move sqlite database outside of main source tree.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,8 +6,25 @@ from contextlib import contextmanager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import staleness_of
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import sys
+
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://'+arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url        
+    
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+            
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -27,7 +44,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         )
     
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         
         self.assertIn('To-Do',self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -64,7 +81,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         
         #now francis visit the home.html
         #to test one can't get others' lists
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers',page_text)
         self.assertNotIn('make a fly',page_text)
@@ -82,10 +99,10 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn('Buy peacock feathers',page_text)
         self.assertNotIn('make a fly',page_text)
         
-        self.fail("Finish the test!")
+        #self.fail("Finish the test!")
         
     def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024,768)
         
         inputbox = self.browser.find_element_by_id('id_new_item')
